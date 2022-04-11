295b6cf7e586d999e6e9b5111bb869564bac0230
Soumik Dutta <shalearkane@gmail.com>
2022-04-11 13:43:18 +0530

    added pso

--- src/org/maps/Main.java
+++ src/org/maps/Main.java
@@ -11,6 +11,10 @@ public class Main {
     public static void main(String[] args) {
         Inputs.generate_dependency_table();
         Swarm ss = new Swarm();
+        for(int i = 0; i<100; i++) {
+            ss.proceed_generation();
+            ss.print_gbest();
+        }
     }
     // (1 : 3), (3 : 3), (4 : 2), (2 : 1), (5 : 3), (6 : 2), (9 : 2), (7 : 3), (8 : 1), (10 : 2),
 }
--- src/org/maps/PSO/Swarm.java
+++ src/org/maps/PSO/Swarm.java
@@ -1,9 +1,6 @@
 package org.maps.PSO;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
-import java.util.Vector;
 
 import static org.maps.InputData.Constants.*;
 
@@ -11,30 +8,31 @@ public class Swarm {
     Particle[] swarm = new Particle[MAX_SWARM];
     Particle[] pbest = new Particle[MAX_SWARM];
     Particle gbest = new Particle();
+
     public Swarm() {
         gbest.fitness = Integer.MAX_VALUE;
-        for(int i = 1; i<= MAX_SWARM; i++) {
+        for (int i = 1; i <= MAX_SWARM; i++) {
             Particle c_rand = new Particle();
             c_rand.generate();
-            swarm[i-1] = c_rand;
-            pbest[i-1] = c_rand;
-            if(gbest.fitness > c_rand.fitness) {
+            swarm[i - 1] = c_rand;
+            pbest[i - 1] = c_rand;
+            if (gbest.fitness > c_rand.fitness) {
                 gbest = c_rand;
             }
         }
     }
 
     private void update_pbest() {
-        for(int i = 0; i<MAX_SWARM; i++) {
-            if(swarm[i].fitness < pbest[i].fitness) {
+        for (int i = 0; i < MAX_SWARM; i++) {
+            if (swarm[i].fitness < pbest[i].fitness) {
                 pbest[i] = swarm[i];
             }
         }
     }
 
     private void update_gbest() {
-        for(int i = 0; i<MAX_SWARM; i++) {
-            if(swarm[i].fitness < gbest.fitness) {
+        for (int i = 0; i < MAX_SWARM; i++) {
+            if (swarm[i].fitness < gbest.fitness) {
                 gbest = swarm[i];
             }
         }
@@ -42,26 +40,66 @@ public class Swarm {
 
     public void proceed_generation() {
         Random random = new Random();
-        for(int i = 0; i<MAX_SWARM;i++) {
-            for(int j = 1; j<= MAX_TASKS; j++) {
+        for (int i = 0; i < MAX_SWARM; i++) {
+            for (int j = 1; j <= MAX_TASKS; j++) {
                 // compare with pbest
-                if(swarm[i].gene[j].processor == pbest[i].gene[j].processor) {
+                if (swarm[i].gene[j].processor == pbest[i].gene[j].processor) {
                     // reduce velocity
-                    swarm[i].velocity[j] -= random.nextFloat(1)*SWARM_C1;
+                    swarm[i].velocity[j] -= random.nextFloat(1) * SWARM_C1;
                 } else {
                     // increase velocity
-                    swarm[i].velocity[j] += random.nextFloat(1)*SWARM_C1;
+                    swarm[i].velocity[j] += random.nextFloat(1) * SWARM_C1;
                 }
 
                 // compare with gbest
-                if(swarm[i].gene[j].processor == gbest.gene[j].processor) {
+                if (swarm[i].gene[j].processor == gbest.gene[j].processor) {
                     // reduce velocity
-                    swarm[i].velocity[j] -= random.nextFloat(1)*SWARM_C2;
+                    swarm[i].velocity[j] -= random.nextFloat(1) * SWARM_C2;
                 } else {
                     // increase velocity
-                    swarm[i].velocity[j] += random.nextFloat(1)*2;
+                    swarm[i].velocity[j] += random.nextFloat(1) * SWARM_C2;
                 }
             }
         }
+
+        // swap velocities
+        for (Particle p : swarm) {
+            int idx_max_2 = -1;
+            int idx_max_1 = -1;
+            float idx_max_vel_1 = Float.MIN_VALUE;
+            float idx_max_vel_2 = Float.MIN_VALUE;
+            for (int i = 1; i <= MAX_TASKS; i++) {
+                if (p.velocity[i] > idx_max_vel_2) {
+                    if (p.velocity[i] > idx_max_vel_1) {
+                        // update second best values
+                        idx_max_vel_2 = idx_max_vel_1;
+                        idx_max_2 = idx_max_1;
+
+                        // update first best values
+                        idx_max_vel_1 = p.velocity[i];
+                        idx_max_1 = i;
+                    } else {
+                        // update second best values
+                        idx_max_vel_2 = p.velocity[i];
+                        idx_max_2 = i;
+                    }
+                }
+            }
+
+            // swap vms of top two velocities
+
+            System.out.println(idx_max_1 + " : " + idx_max_2);
+            final int vm_temp = p.gene[idx_max_1].processor;
+            p.gene[idx_max_1].processor = p.gene[idx_max_2].processor;
+            p.gene[idx_max_2].processor = vm_temp;
+            p.calculate_details();
+        }
+        update_pbest();
+        update_gbest();
+    }
+
+    public void print_gbest() {
+        gbest.calculate_details();
+        gbest.print_details();
     }
 }
